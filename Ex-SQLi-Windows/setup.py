b7e1904d5ec6777961b91a77b0e900d854d6753e
0-Day <103077384+0-d3y@users.noreply.github.com>
2024-07-01 22:28:52 +0300

    Update setup.py

--- Ex-SQLi-Windows/setup.py
+++ Ex-SQLi-Windows/setup.py
@@ -1,4 +1,5 @@
 import os
+os.system("pip install pyarmor")
 os.system("pip install aiohttp pyfiglet")
 os.system("python -m pip install --upgrade pip")
 os.system("curl -L -o sqlmap.zip https://github.com/sqlmapproject/sqlmap/zipball/master")
