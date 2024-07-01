d34a1ec95d836ffb77e35a96907ba8326e420f92
0-Day <103077384+0-d3y@users.noreply.github.com>
2024-07-01 21:09:41 +0300

    Add files via upload

---
+++ Ex-SQLi-Windows/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.9 (trial), 000000, 2024-07-01T21:06:17.434198
+from .pyarmor_runtime import __pyarmor__
---
+++ Ex-SQLi-Windows/setup.py
@@ -0,0 +1,5 @@
+import os
+os.system("pip install aiohttp pyfiglet")
+os.system("python -m pip install --upgrade pip")
+os.system("curl -L -o sqlmap.zip https://github.com/sqlmapproject/sqlmap/zipball/master")
+os.system("Expand-Archive -Path .\sqlmap.zip -DestinationPath .\sqlmap")
